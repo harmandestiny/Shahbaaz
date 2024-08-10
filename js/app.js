a5c88f71711eda2b412deb53a748d3bf1a11039a
harmandestiny <102397804+harmandestiny@users.noreply.github.com>
2024-08-10 21:44:37 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 
 
 $(document).ready(function(){
-    $('.video-slider').slick({
-    slidesToShow: 1,
+  $('.video-slider').slick({
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    prevArrow:".prev-btn",
-    nextArrow:".next-btn",
-  })
-})
+    prevArrow: ".prev-btn",
+    nextArrow: ".next-btn",
+    responsive: [
+      {
+        breakpoint: 360, // Adjust this value to the maximum width of your mobile view
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
 document.getElementById('hamburger').addEventListener('click', function () {
   var navLinks = document.getElementById('nav-links');
   navLinks.classList.toggle('active');
